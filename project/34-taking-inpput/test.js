d358f11d34a98f6d9f6eff7ce850832bdd7b75cb
htehrani98 <h.tehrani1377@gmail.com>
2022-04-04 14:25:27 +0430

    idk

--- project/34-taking-inpput/test.js
+++ project/34-taking-inpput/test.js
@@ -1,12 +1,14 @@
 function validate() {
   const username = document.getElementById('username')
   const password = document.getElementById('password')
+  const message = document.getElementsById('message')
   if (username.value.trim() === '' || password.value.trim() === '') {
+    message.innerText = "you must fill all input"
+    alert('you must fill all input')
     return false
   }
   else {
+    message.innerText = ""
     return true
   }
-  
-
 }
